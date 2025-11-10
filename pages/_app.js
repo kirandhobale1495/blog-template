4b8dad25019f9b7c0fddaf984e7d330b0d538745
Kiran Dhobale <kirandhobale1495@users.noreply.github.com>
2025-11-10 15:33:24 +0530

    Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout";
+import Analytics from "./Analytics";
 
 export default function App({ Component, pageProps, showHeader }) {
   return (
-    <Layout pageTitle="Kiran Dhobale" description="My Personal Blog" showHeader>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+    <Analytics />
+      <Layout pageTitle="Kiran Dhobale" description="My Personal Blog" showHeader>
+        <Component {...pageProps} />
+     </Layout>
+    </>
+    
   );
 }
