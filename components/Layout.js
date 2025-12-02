793bed3a24f669987531f2aec85673cb86ef8986
Kiran Dhobale <kirandhobale1495@users.noreply.github.com>
2025-12-02 19:36:03 +0530

    Resolve accessibility issues

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ export default function Layout({ children, pageTitle, description, showHeader = 
   return (
     <>
       <Head>
+        <html lang="en" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="Description" content={description}></meta>
